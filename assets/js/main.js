2399018098e489bf1ba939861a0bcd0f9db4ae40
jxcobswipefile <jxcobswipefile@gmail.com>
2025-10-29 13:48:50 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,13 +23,13 @@
   // ---------------------------- Data (guarded) ----------------------------
   // Product cards used on general pages (small list)
   AFP.ITEMS = AFP.ITEMS || [
-    { slug: "panasonic-tz",          name: "Panasonic TZ",          img: "assets/img/products/panasonic-tz/hero.jpg",          url: "products/panasonic-tz.html",          cool_area_m2: 25 },
-    { slug: "daikin-comfora",        name: "Daikin Comfora",        img: "assets/img/products/daikin-comfora/hero.jpg",        url: "products/daikin-comfora.html",        cool_area_m2: 25 },
-    { slug: "haier-revive-plus",     name: "Haier Revive Plus",     img: "assets/img/products/haier-revive-plus/hero.jpg",     url: "products/haier-revive-plus.html",     cool_area_m2: 25 },
-    { slug: "daikin-perfera",        name: "Daikin Perfera",        img: "assets/img/products/daikin-perfera/hero.jpg",        url: "products/daikin-perfera.html",        cool_area_m2: 25 },
-    { slug: "panasonic-etherea",     name: "Panasonic Etherea",     img: "assets/img/products/panasonic-etherea/hero.jpg",     url: "products/panasonic-etherea.html",     cool_area_m2: 25 },
-    { slug: "haier-expert",          name: "Haier Expert",          img: "assets/img/products/haier-expert/hero.jpg",          url: "products/haier-expert.html",          cool_area_m2: 26 },
-    { slug: "daikin-emura",          name: "Daikin Emura",          img: "assets/img/products/daikin-emura/hero.jpg",          url: "products/daikin-emura.html",          cool_area_m2: 25 }
+    { slug: "panasonic-tz",          name: "Panasonic TZ",          img: "assets/indoor units kh/panasonic tz indoor.jpg",          url: "products/panasonic-tz.html",          cool_area_m2: 25 },
+    { slug: "daikin-comfora",        name: "Daikin Comfora",        img: "assets/indoor units kh/daikin comfora indoor.jpg",        url: "products/daikin-comfora.html",        cool_area_m2: 25 },
+    { slug: "haier-revive-plus",     name: "Haier Revive Plus",     img: "assets/indoor units kh/haier revive indoor.jpg",     url: "products/haier-revive-plus.html",     cool_area_m2: 25 },
+    { slug: "daikin-perfera",        name: "Daikin Perfera",        img: "assets/indoor units kh/daikin perfera indoor.jpg",        url: "products/daikin-perfera.html",        cool_area_m2: 25 },
+    { slug: "panasonic-etherea",     name: "Panasonic Etherea",     img: "assets/indoor units kh/panasonic etherea indoor.jpg",     url: "products/panasonic-etherea.html",     cool_area_m2: 25 },
+    { slug: "haier-expert",          name: "Haier Expert",          img: "assets/indoor units kh/haier expert indoor.jpg",          url: "products/haier-expert.html",          cool_area_m2: 26 },
+    { slug: "daikin-emura",          name: "Daikin Emura",          img: "assets/indoor units kh/daikin emura indoor.jpg",          url: "products/daikin-emura.html",          cool_area_m2: 25 }
   ];
 
   // Variants used by the recommender (capacity-based)
